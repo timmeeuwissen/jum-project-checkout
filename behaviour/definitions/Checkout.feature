8cb6d4c8a2184722e3d820402293d538ff6fa2ff
Tim Meeuwissen <tim.meeuwissen@gmail.com>
2019-10-21 11:12:55 +0200

    Added stuff for laura to have a look at

---
+++ behaviour/definitions/Checkout.feature
@@ -0,0 +1,8 @@
+Feature: The checkout page
+
+   The checkout page was taken as an example for 
+   developing together on one platform.
+
+    Scenario: Opening the checkout page
+        Given I navigate to the checkout page
+        Then I expect a header saying "Todos"
---
+++ behaviour/definitions/Checkout/checkout.js
@@ -0,0 +1,15 @@
+// import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
+
+// const urlMap = {
+//   'checkout page': '/checkout/checkout',
+// };
+
+// Given(/^I navigate to the (.*)$/, target => {
+//   cy.log('Given is running');
+// //   cy.visit(urlMap[target]);
+//   cy.visit('/checkout/checkout');
+// });
+
+// Then(/I expect a header saying "(.*)"/, headerText => {
+//   cy.get('h2').should('have.text', headerText);
+// });
